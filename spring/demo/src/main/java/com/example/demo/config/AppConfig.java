e31c9f246a2db5ed543a3d572ff388144e49550c
Hà Mạnh Cường test <hamanhcuong18112004@gmail.com>
2026-03-16 13:36:25 +0700

    spring singleton pattern

---
+++ spring/demo/src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,20 @@
+
+public class AppConfig {
+    private static AppConfig instance;
+    private final Map<String, Object> configs = new HashMap<>();
+
+    private AppConfig() {
+        configs.put("dbHost", "localhost");
+        configs.put("dbPort", 5432);
+        configs.put("shopName", "Highland Coffee");
+        configs.put("maxOrders", 100);
+    }
+
+    public static synchronized AppConfig getInstance() {
+        if (instance == null) instance = new AppConfig();
+        return instance;
+    }
+
+    public Object getConfig(String key) { return configs.get(key); }
+    public void setConfig(String key, Object value) { configs.put(key, value); }
+}
